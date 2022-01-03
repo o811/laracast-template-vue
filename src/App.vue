6e3086dd78eac8f1e3846e21aea10bb062a6fca1
Oleksii Huzchenko <oleksiihuzchenko@MacBook-Pro-Oleksii.local>
2022-01-03 18:12:42 +0100

    Initial project commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <nav>
+        <div class="logo">
+        <img src="./assets/logo.png" width="50" height="50">
+        </div>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+      <section></section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
